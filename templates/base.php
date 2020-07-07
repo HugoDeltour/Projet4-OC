d1441a96d7bab69a627b24485cebf99780163e80
Hugo DELOUR <deltourh@hotmail.fr>
2020-07-07 14:58:21 +0200

    Configuration de TinyMCE

--- templates/base.php
+++ templates/base.php
@@ -2,7 +2,8 @@
 <html lang=fr>
 <head>
   <meta charset="utf-8"/>
-  <script src="https://cdn.tiny.cloud/1/lsw4pp1qn3tm8vq4388zl14s885ym08qmbt8acysik0qhmo6/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+  <script src="../config/tinyMCE/js/tinyMCE/tinymce.min.js" type="text/javascript"></script>
+  <script src="../public/js/tinyMCE.js" type="text/javascript"></script>
   <title><?=$title?></title>
 </head>
 <body>
