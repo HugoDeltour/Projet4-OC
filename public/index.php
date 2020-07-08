40535887d4a4f5a64f89cee0a03dd447d9ea5f49
Hugo DELOUR <deltourh@hotmail.fr>
2020-07-08 16:01:19 +0200

    Essaie tinyMCE

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require '../config/dev.php';
 require '../vendor/autoload.php';
+require '../config/tinyMCE/tinymce/js/tinymce/tinymce.min.js';
 session_start();
 $router = new App\config\router();
 $router->run();
